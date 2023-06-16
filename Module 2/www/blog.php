47b0277e10bcc32e53beb8ef18b2a3e12e4d8f50
LindezaGrey <39629455+LindezaGrey@users.noreply.github.com>
2023-06-16 21:14:06 +0000

    🐛 fix title

--- Module 2/www/blog.php
+++ Module 2/www/blog.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>News</title>
+    <title>Blog</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <script src='main.js'></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
