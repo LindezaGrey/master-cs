05a7f61c1b2765ce1dcc1f0ed78a67e54a61c8c9
LindezaGrey <39629455+LindezaGrey@users.noreply.github.com>
2024-07-02 13:04:15 +0000

    ✨ w2

--- Module 9/excercises/week_2/chess.py
+++ Module 9/excercises/week_2/chess.py
@@ -1,8 +1,6 @@
-import turtle
 import pprint
-import matplotlib
-
-matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 
 START_X = -320
 START_Y = 320
@@ -13,4 +11,20 @@ for row in range(BOARD_SIZE):
     for col in range(BOARD_SIZE):
         coords.append((START_X + row * 40, START_Y - col * 40))
 
+
+# use matplotlib to draw the chess board
+def draw_chess_board(coords: list[tuple[float, float]]):
+    fig, ax = plt.subplots()
+    for coord in coords:
+        # condition holds true for every second field in either direction
+        color = "black" if (coord[0] + coord[1]) % 80 == 0 else "white"
+        rect = Rectangle(coord, 40, 40, edgecolor="black", facecolor=color)
+        ax.add_patch(rect)
+    plt.axis("equal")
+    plt.axis("off")
+
+    plt.savefig("chess_board.png")
+
+
 pprint.pprint(coords)
+draw_chess_board(coords)
